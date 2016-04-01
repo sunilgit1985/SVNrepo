e87eafb98c3c894c5a57ffc35a93f7baca8cb56e
pmehta <pmehta@bd0249eb-a025-43e3-b6fa-fed758350ba6>
2016-04-01 20:27:51 +0000

    Introduced new Fixed Model

---
+++ invessence/modules/broker-services/src/main/java/com/invessence/broker/util/FileEmptyException.java
@@ -0,0 +1,12 @@
+package com.invessence.broker.util;
+
+/**
+ * Created by abhangp on 1/22/2016.
+ */
+public class FileEmptyException extends Exception
+{
+   public FileEmptyException(String message){
+      super(message);
+   }
+
+}
