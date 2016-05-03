01bcc5fef2ed964556282477e29c1d93032ef0fb
abhangp <abhangp@bd0249eb-a025-43e3-b6fa-fed758350ba6>
2016-05-03 15:46:52 +0000

    LTAM version 3,0 - Introducing Web-Service integration

--- ltam/modules/broker-webservices/src/main/java/com/invessence/MainPendingAccCreation.java
+++ ltam/modules/broker-webservices/src/main/java/com/invessence/MainPendingAccCreation.java
@@ -1,27 +1,27 @@
-//package com.invessence;
-//
-//import org.springframework.context.ConfigurableApplicationContext;
-//import org.springframework.context.support.ClassPathXmlApplicationContext;
-//
-///**
-// * Created by abhangp on 3/11/2016.
-// */
-//public class MainPendingAccCreation
-//{
-//   public static void main(String[] args)
-//   {
-//      try
-//      {
-//
-//         ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-//         CallingProgramme callingProgramme = (CallingProgramme) context.getBean("callingProgramme");
-//         callingProgramme.createPendingWebUser();
-//         context.close();
-//      }
-//      catch (Exception e)
-//      {
-//         e.printStackTrace();
-//      }
-//
-//   }
-//}
+package com.invessence;
+
+import com.invessence.ws.service.ServiceLayerImpl;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by abhangp on 3/11/2016.
+ */
+public class MainPendingAccCreation
+{
+   public static void main(String[] args)
+   {
+      try
+      {
+         ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("webServicesConfig.xml");
+         ServiceLayerImpl serviceLayer = (ServiceLayerImpl) context.getBean("serviceLayerImpl");
+         serviceLayer.createPendingUser();
+         context.close();
+      }
+      catch (Exception e)
+      {
+         e.printStackTrace();
+      }
+
+   }
+}
