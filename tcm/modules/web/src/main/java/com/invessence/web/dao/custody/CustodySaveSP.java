ebf3c7b644bc00149c3f04ee867a5e64ea438010
sudhir <sudhir@bd0249eb-a025-43e3-b6fa-fed758350ba6>
2016-09-06 17:57:26 +0000

    Client account opening - Sudhir

--- tcm/modules/web/src/main/java/com/invessence/web/dao/custody/CustodySaveSP.java
+++ tcm/modules/web/src/main/java/com/invessence/web/dao/custody/CustodySaveSP.java
@@ -1,10 +1,16 @@
 package com.invessence.web.dao.custody;
 
+import java.sql.Types;
 import java.util.*;
 import javax.sql.DataSource;
 
 import com.invessence.web.data.custody.*;
+import org.springframework.jdbc.core.*;
 import org.springframework.jdbc.object.StoredProcedure;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
+import java.util.Date;
 
 /**
  * Created with IntelliJ IDEA.
@@ -35,6 +41,20 @@ public class CustodySaveSP extends StoredProcedure
          case 6:
          case 7:
          case 8:
+
+            declareParameter(new SqlParameter("p_acctnum", Types.BIGINT));
+            declareParameter(new SqlParameter("p_beneId", Types.INTEGER));
+            declareParameter(new SqlParameter("p_beneFirstName", Types.VARCHAR));
+            declareParameter(new SqlParameter("p_beneMidInitial", Types.VARCHAR));
+            declareParameter(new SqlParameter("p_beneLastName", Types.VARCHAR));
+            declareParameter(new SqlParameter("p_beneSSN", Types.VARCHAR));
+            declareParameter(new SqlParameter("p_beneDOB", Types.DATE));
+            declareParameter(new SqlParameter("p_beneRel", Types.VARCHAR));
+            declareParameter(new SqlParameter("p_typeOfBeneficiary", Types.VARCHAR));
+            declareParameter(new SqlParameter("p_perStripes", Types.VARCHAR));
+            declareParameter(new SqlParameter("p_sharePerc", Types.DOUBLE));
+            declareParameter(new SqlParameter("p_createdBy", Types.VARCHAR));
+
          case 9:
             break;
          default:
@@ -53,8 +73,30 @@ public class CustodySaveSP extends StoredProcedure
       return super.execute(inputMap);
    }
 
-   public Map td_saveAccountOwner(AcctOwnersDetails data) {
+   public Map td_saveAccountOwner(AcctOwnersDetails data)
+   {
       Map inputMap = new HashMap();
+      return super.execute(inputMap);
+   }
+
+   public Map td_saveBenefiaciaryDetails(BenefiaciaryDetails data) {
+
+
+
+      Map inputMap = new HashMap();
+      inputMap.put("p_acctnum", data.getAcctnum());
+      inputMap.put("p_beneId", data.getBeneId());
+      inputMap.put("p_beneFirstName", data.getBeneFirstName());
+      inputMap.put("p_beneMidInitial", data.getBeneMidInitial());
+      inputMap.put("p_beneLastName", data.getBeneLastName());
+      inputMap.put("p_beneSSN", data.getBeneSSN());
+      inputMap.put("p_beneDOB", "2001-01-01");
+      inputMap.put("p_beneRel", data.getBeneRel());
+      inputMap.put("p_typeOfBeneficiary", data.getTypeOfBeneficiary());
+      inputMap.put("p_perStripes", data.getPerStripes());
+      inputMap.put("p_sharePerc", data.getSharePerc());
+      inputMap.put("p_createdBy", data.getCreatedBy());
+
       return super.execute(inputMap);
    }
 }
