5fd671e3603eadb7483906e9e972fb02f6c3e6af
abhangp <abhangp@bd0249eb-a025-43e3-b6fa-fed758350ba6>
2016-11-08 13:40:51 +0000

    Abhang Checked in 11/08/2016

--- Invessence/DBScripts/Release_20161112/1_insert_into_service_schema.sql
+++ Invessence/DBScripts/Release_20161112/1_insert_into_service_schema.sql
@@ -48,3 +48,12 @@ INSERT INTO service.dc_template_mapping VALUES
 
 INSERT INTO service.service_operation_details VALUES
 ('BUILDINGBENJAMINS','DOCUSIGN-SERVICES','ACAT_OTHER_NEW','DOCUSIGN','A',0,'BB_ACAT_OTHER');
+
+ALTER TABLE service.dc_document_details
+ADD COLUMN docSeq INT NULL AFTER docType;
+
+
+INSERT INTO service.dc_document_details (company, service, docCode, docName, docType, docSeq, fileName, editable, status) VALUES ('BUILDINGBENJAMINS', 'DOCUSIGN-SERVICES', 'TD_PRIVACY_POLICY', 'Building Benjamins TCM Privacy Notice', 'PDF', '3', 'Building Benjamins TCM Privacy Notice.pdf', 'N', 'A');
+UPDATE service.dc_document_details SET docSeq='1' WHERE company='BUILDINGBENJAMINS' and service='DOCUSIGN-SERVICES' and docCode='TD_ADV_PART_2A';
+UPDATE service.dc_document_details SET docName='TRADITION CAPITAL MANAGEMENT - CRD 113755 - ADV PART 2B', docSeq='2', fileName='TRADITION CAPITAL MANAGEMENT - CRD 113755 - ADV PART 2B.pdf' WHERE company='BUILDINGBENJAMINS' and service='DOCUSIGN-SERVICES' and docCode='TD_ADV_PART_2B';
+UPDATE service.dc_document_details SET docSeq='4' WHERE company='BUILDINGBENJAMINS' and service='DOCUSIGN-SERVICES' and docCode='TD_ADV_AGREEMENT';
