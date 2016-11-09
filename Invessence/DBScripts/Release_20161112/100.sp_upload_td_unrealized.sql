5fd9fb58d8b94937ebafb7aa47c0890ff82ee00a
pmehta <pmehta@bd0249eb-a025-43e3-b6fa-fed758350ba6>
2016-11-09 03:36:06 +0000

    Checked in 11/06/2016

--- Invessence/DBScripts/Release_20161112/100.sp_upload_td_unrealized.sql
+++ Invessence/DBScripts/Release_20161112/100.sp_upload_td_unrealized.sql
@@ -5,14 +5,18 @@ CREATE PROCEDURE `temp`.`sp_upload_td_unrealized`(
 )
 BEGIN
 
-	DECLARE t_reportDate	VARCHAR(10);
-    
+	DECLARE tReportDate	VARCHAR(10);
+
+    SELECT MAX(`invdb`.`funct_strdate2inv_date`(`tmp_td_unrealized`.`businessDate`, '%m/%d/%Y'))
+    INTO tReportDate
+    FROM `temp`.`tmp_td_unrealized`;
+
     -- First delete all position for this business date
 	DELETE FROM `invdb`.`ext_position`
-    where reportDate in (select distinct `invdb`.`funct_strdate2inv_date`(`tmp_td_unrealized`.`businessDate`, '%m/%d/%Y') 
+    where reportDate in (select distinct `invdb`.`funct_strdate2inv_date`(`tmp_td_unrealized`.`businessDate`, '%m/%d/%Y')
 					     from `temp`.`tmp_td_unrealized`)
     ;
-    
+
 	INSERT INTO `invdb`.`ext_position`
 	(`acctnum`,
 	`clientAccountID`,
@@ -41,23 +45,63 @@ BEGIN
 		 , `tmp_td_unrealized`.`currentQuantity`
 		 , CASE WHEN (`tmp_td_unrealized`.`currentQuantity` is null) THEN 1
 				WHEN (`tmp_td_unrealized`.`currentQuantity` = 0) THEN 1
-                ELSE (`tmp_td_unrealized`.`bookCost` / `tmp_td_unrealized`.`currentQuantity`) 
+                ELSE (`tmp_td_unrealized`.`costBasis` / `tmp_td_unrealized`.`currentQuantity`)
 			END costBasisPrice
-		 , `tmp_td_unrealized`.`bookCost` as `costBasisMoney`
-		 , IFNULL(`tmp_td_price`.price, 
-				CASE WHEN (`tmp_td_unrealized`.`currentQuantity` is null) THEN 1
+		 , `tmp_td_unrealized`.`costBasis` as `costBasisMoney`
+		 , CASE WHEN (`tmp_td_unrealized`.`currentQuantity` is null) THEN 1
 				WHEN (`tmp_td_unrealized`.`currentQuantity` = 0) THEN 1
-                ELSE (`tmp_td_unrealized`.`bookCost` / `tmp_td_unrealized`.`currentQuantity`) 
-				END) as markPrice
-		 ,  (IFNULL(`tmp_td_price`.price,1) * IFNULL(`tmp_td_unrealized`.`currentQuantity`,1)) as `positionValue`
-		 ,  (IFNULL(`tmp_td_price`.price,1) * IFNULL(`tmp_td_unrealized`.`currentQuantity`,1)) - `tmp_td_unrealized`.`bookCost` as pnlUnrealized
+                ELSE (`tmp_td_unrealized`.`costBasis` / `tmp_td_unrealized`.`currentQuantity`)
+			END markPrice
+		 , `tmp_td_unrealized`.`costBasis` as `positionValue`
+		 , 0 as pnlUnrealized
 		 , `tmp_td_unrealized`.`ID` as `levelOfDetail`
 		 , now() as created
-    FROM `invdb`.`ext_acct_info`
-		 INNER JOIN `temp`.`tmp_td_unrealized`
-         ON (`ext_acct_info`.`clientAccountID` = `tmp_td_unrealized`.`accountNumber`)
-		 LEFT JOIN `temp`.`tmp_td_price`
-         ON (`tmp_td_unrealized`.`symbolCUSIP` = `tmp_td_price`.`symbolCUSIP`)
+    FROM `invdb`.`ext_acct_info` as `ext_acct_info`, `temp`.`tmp_td_unrealized` as `tmp_td_unrealized`
+    WHERE `ext_acct_info`.`clientAccountID` = `tmp_td_unrealized`.`accountNumber`
 	;
+
+    -- Now Add Cash Position.  NOTE:  We deleted all records at top.
+	INSERT INTO `invdb`.`ext_position`
+		(`acctnum`,
+		`clientAccountID`,
+		`currencyPrimary`,
+		`fxRateToBase`,
+		`symbol`,
+		`reportDate`,
+		`purchaseDate`,
+		`side`,
+		`quantity`,
+		`costBasisPrice`,
+		`costBasisMoney`,
+		`markPrice`,
+		`positionValue`,
+		`pnlUnrealized`,
+		`levelOfDetail`,
+		`created`)
+    SELECT `ext_acct_info`.`acctnum`
+		 , `ext_acct_info`.`clientAccountID`
+         , 'USD' as currencyPrimary
+         , 1.0 as fxRateToBase
+         , 'Cash' as Symbol
+         , tReportDate as `businessdate`
+         , tReportDate as `originalPurchaseDate`
+         , 'Long' as `side`
+		 , SUM(`tmp_td_position`.`amount`) as `currentQuantity`
+		 , 1 as costBasisPrice
+		 , SUM(`tmp_td_position`.`amount`) as `costBasisMoney`
+		 , 1 as markPrice
+		 , SUM(`tmp_td_position`.`amount`) as `positionValue`
+		 , 0 as pnlUnrealized
+		 , 'Cash' as `levelOfDetail`
+		 , now() as created
+    FROM `invdb`.`ext_acct_info` as `ext_acct_info`, `temp`.`tmp_td_position` as `tmp_td_position`
+    WHERE `ext_acct_info`.`clientAccountID` = `tmp_td_position`.`accountNumber`
+    AND   `tmp_td_position`.`securityType` in ('Cash', 'MF', 'Money')
+    GROUP BY
+			`ext_acct_info`.`acctnum`
+		 , `ext_acct_info`.`clientAccountID`
+
+	;
+
 END$$
 DELIMITER ;
