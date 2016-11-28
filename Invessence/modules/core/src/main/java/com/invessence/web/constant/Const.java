e31bc4385fa0319a35db560eb701768de7f3f425
abhangp <abhangp@bd0249eb-a025-43e3-b6fa-fed758350ba6>
2016-11-28 15:02:11 +0000

    Abhang Checked in 11/28/2016

--- Invessence/modules/core/src/main/java/com/invessence/web/constant/Const.java
+++ Invessence/modules/core/src/main/java/com/invessence/web/constant/Const.java
@@ -2,7 +2,7 @@ package com.invessence.web.constant;
 
 public class Const
 {
-   public final static String COMPANY_NAME = "Invessence";
+   public final static String COMPANY_NAME = "BUILDINGBENJAMINS";//"Invessence";
 
    //public final static String WEBSITE_URL= "http://invessence.com";
    public static String MAIL_SENDER = "noreply@invessence.com";
