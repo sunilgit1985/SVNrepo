79e77c76bc570716fe92b6870d46cb2e9f018b4c
pmehta <pmehta@bd0249eb-a025-43e3-b6fa-fed758350ba6>
2016-12-03 02:20:39 +0000

    Prashant Committed 12/01/2016

--- Invessence/modules/core/src/main/java/com/invessence/web/constant/Const.java
+++ Invessence/modules/core/src/main/java/com/invessence/web/constant/Const.java
@@ -2,7 +2,7 @@ package com.invessence.web.constant;
 
 public class Const
 {
-   public final static String COMPANY_NAME = "UOB";//"Invessence";
+   public final static String COMPANY_NAME = "Invessence";//"Invessence";
 
    //public final static String WEBSITE_URL= "http://invessence.com";
    public static String MAIL_SENDER = "noreply@invessence.com";
