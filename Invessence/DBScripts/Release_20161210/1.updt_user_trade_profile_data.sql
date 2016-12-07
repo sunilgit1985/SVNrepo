b50f16d3279d9c3c03ca2b13643f52c02010afe7
pmehta <pmehta@bd0249eb-a025-43e3-b6fa-fed758350ba6>
2016-12-07 04:16:18 +0000

    Prashant Committed 12/06/2016

---
+++ Invessence/DBScripts/Release_20161210/1.updt_user_trade_profile_data.sql
@@ -0,0 +1,16 @@
+update `invdb`.`user_trade_profile`, `invdb`.`dc_acct_details`, `invdb`.`dc_m_lookup`
+ set `user_trade_profile`.`acctType` = upper(`dc_m_lookup`.`displayName`)
+WHERE `dc_acct_details`.`acctnum` = `user_trade_profile`.`acctnum`
+AND   `dc_acct_details`.`acctTypeId` = `dc_m_lookup`.`lookupCode` 
+AND   `dc_m_lookup`.`lookupSet` = 'ACCTTYPE'
+AND   `user_trade_profile`.`managed` not in ('N')
+;
+
+update `invdb`.`user_trade_profile`
+	set `status` = `managed`
+;
+
+update `invdb`.`user_trade_profile`
+	set `managed` = 'A'
+where `managed` not in ('N')
+;
