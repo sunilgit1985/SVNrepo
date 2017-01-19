6a615b805cbce7ed55c77006def518771d2dc269
akhilesh <akhilesh@bd0249eb-a025-43e3-b6fa-fed758350ba6>
2017-01-19 13:00:28 +0000

    Checked in 01/06/2017 Ticket 209,215,273,283,173

---
+++ Invessence/DBScripts/Release-20170204/6 - web_site_info insertScript for Fevicon.sql
@@ -0,0 +1,3 @@
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('invessence', 'WEB.FAVICONLOGO', 'A', '/javax.faces.resource/images/BBicon.png.xhtml?ln=tcm', 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uob', 'WEB.FAVICONLOGO', 'A', '/javax.faces.resource/images/Invessenceicon.png.xhtml?ln=tcm', 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('buildingbenjamins', 'WEB.FAVICONLOGO', 'A', '/javax.faces.resource/images/UOBicon.png.xhtml?ln=tcm', 'N', now(), null);
