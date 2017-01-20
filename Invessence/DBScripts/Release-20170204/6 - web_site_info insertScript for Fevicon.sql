9cbd97c17d9a3f7cd1042e4683939f820fc4e858
abhangp <abhangp@bd0249eb-a025-43e3-b6fa-fed758350ba6>
2017-01-20 07:58:07 +0000

    Checked in 01/06/2017 Ticket 209,215,273,283,173

--- Invessence/DBScripts/Release-20170204/6 - web_site_info insertScript for Fevicon.sql
+++ Invessence/DBScripts/Release-20170204/6 - web_site_info insertScript for Fevicon.sql
@@ -1,3 +1,10 @@
 INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('invessence', 'WEB.FAVICONLOGO', 'A', '/javax.faces.resource/images/BBicon.png.xhtml?ln=tcm', 'N', now(), null);
 INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uob', 'WEB.FAVICONLOGO', 'A', '/javax.faces.resource/images/Invessenceicon.png.xhtml?ln=tcm', 'N', now(), null);
 INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('buildingbenjamins', 'WEB.FAVICONLOGO', 'A', '/javax.faces.resource/images/UOBicon.png.xhtml?ln=tcm', 'N', now(), null);
+
+
+
+UPDATE `invdb`.`web_site_info` SET `value`='/javax.faces.resource/images/Invessenceicon.png.xhtml?ln=tcm' WHERE `url`='invessence' and`name`='WEB.FAVICONLOGO';
+UPDATE `invdb`.`web_site_info` SET `value`='/javax.faces.resource/images/BBicon.png.xhtml?ln=tcm' WHERE `url`='buildingbenjamins' and`name`='WEB.FAVICONLOGO';
+UPDATE `invdb`.`web_site_info` SET `value`='/javax.faces.resource/images/UOBicon.png.xhtml?ln=tcm' WHERE `url`='uob' and`name`='WEB.FAVICONLOGO';
+
