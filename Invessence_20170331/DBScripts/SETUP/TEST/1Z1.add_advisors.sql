fbc4d4f8265d5c9606514c2aa1366c83d01bf60f
pmehta <pmehta@bd0249eb-a025-43e3-b6fa-fed758350ba6>
2017-04-05 00:29:03 +0000

    Prashant 04/4/2017 - Advisor Creation

---
+++ Invessence_20170331/DBScripts/SETUP/TEST/1Z1.add_advisors.sql
@@ -0,0 +1,65 @@
+CALL `testing`.`invite_advisor`(
+	101, 'michael.advisor','MCiccone@traditioncm.com' ,'Michael','Ciccone'
+	,'BB','101','Tradition Capital Management, LLC','Michael Ciccone','BB'
+	,null,null);
+
+CALL `testing`.`invite_advisor`(
+	102,'ben.advisor','bch@traditioncm.com' ,'Benjamin','Halliburton'
+	,'BB','102','Tradition Capital Management, LLC','Benjamin Halliburton','BB'
+	,null,null);
+
+CALL `testing`.`invite_advisor`(
+	110, 'tcm.advisor','mciccone@candorwealth.com' ,'Michael','Ciccone'
+	,'BB-TCM','101D','Tradition Capital Management, LLC','Michael Ciccone','Direct'
+	,null,null);
+
+CALL `testing`.`invite_advisor`(
+	111,'tcm.advisor.AD','mciccone+ad@candorwealth.com' ,'Michael','Ciccone'
+	,'BB-TCM','101AD','Tradition Capital Management, LLC','Michael Ciccone','Advisor Direct'
+	,null,null);
+
+CALL `testing`.`invite_advisor`(
+	112,'frani.advisor','ffeit@traditioncm.com' ,'Frani','Feit'
+	,'BB-TCM','102D','Tradition Capital Management, LLC','Frani Feit, CFP®','Direct'
+	,null,null);
+
+CALL `testing`.`invite_advisor`(
+	113,'frani.advisor.AD','ffeit+ad@traditioncm.com' ,'Frani','Feit'
+	,'BB-TCM','102AD','Tradition Capital Management, LLC','Frani Feit, CFP®','Advisor Direct'
+	,null,null);
+
+CALL `testing`.`invite_advisor`(
+	114,'chip.advisor','cwieckorek@traditioncm.com' ,'Chip','Wieczorek'
+	,'BB-TCM','103D','Tradition Capital Management, LLC','Chip Wieczorek, CFP®','Direct'
+	,null,null);
+
+CALL `testing`.`invite_advisor`(
+	115,'chip.advisor.AD','rrichardson+ad@traditioncm.com' ,'Chip','Wieczorek'
+	,'BB-TCM','103AD','Tradition Capital Management, LLC','Chip Wieczorek, CFP®','Advisor Direct'
+	,null,null);
+
+CALL `testing`.`invite_advisor`(
+	116,'bobr.advisor','rrichardson@traditioncm.com' ,'Robert','Richardson'
+	,'BB-TCM','104D','Tradition Capital Management, LLC','Robert Richardson','Direct'
+	,null,null);
+
+CALL `testing`.`invite_advisor`(
+	117,'bobr.advisor.AD','rrichardson+ad@traditioncm.com' ,'Robert','Richardson'
+	,'BB-TCM','104AD','Tradition Capital Management, LLC','Robert Richardson','Advisor Direct'
+	,null,null);
+
+CALL `testing`.`invite_advisor`(
+	118,'michael.advisor.D','Mciccone@traditioncm.com' ,'Michael','Ciccone'
+	,'BB-TCM','105D','Tradition Capital Management, LLC','Michael Ciccone','Direct'
+	,null,null);
+
+CALL `testing`.`invite_advisor`(
+	119,'michael.advisor.AD','Mciccone+ad@traditioncm.com' ,'Michael','Ciccone'
+	,'BB-TCM','105AD','Tradition Capital Management, LLC','Michael Ciccone','Advisor Direct'
+	,null,null);
+    
+ CALL `testing`.`invite_advisor`(
+	201,'robk.advisor','rob@traditioncm.com' ,'Robert','Kuzniacki'
+	,'BB-TCM','201','PrePair Financial','Robert Kuzniacki','External'
+	,null,null);
+ 
---
+++ Invessence_20170331/DBScripts/SETUP/TEST/500.pm.dctd_add_template_id.sql
@@ -0,0 +1,103 @@
+DROP PROCEDURE IF EXISTS `invdb`.`dctd_add_template_id`;
+
+DELIMITER $$
+CREATE PROCEDURE `invdb`.`dctd_add_template_id`(
+   p_templateID		VARCHAR(20)
+  ,p_advisorCode	VARCHAR(20)
+  ,p_advisor		VARCHAR(20)
+  ,p_rep			VARCHAR(20)
+  ,p_firmname		VARCHAR(60)
+  ,p_primaryContact VARCHAR(60)
+  ,p_ops_email		VARCHAR(60)
+)
+BEGIN
+
+    DECLARE tTemplateID		BIGINT(20);
+    DECLARE tNewID			BIGINT(20);
+    DECLARE tFunction		VARCHAR(1);
+    
+    IF (p_templateID is NOT NULL)
+    THEN
+		SELECT `dc_advisor_details`.`id`
+        INTO tTemplateID
+		FROM `dc_advisor_details`
+        WHERE `dc_advisor_details`.`primaryContact` = p_templateID
+		AND   `dc_advisor_details`.`advisorName` = p_advisor;		
+	END IF;
+    
+	SELECT  'U'
+	INTO tFunction
+	FROM `dc_advisor_details`
+	WHERE `dc_advisor_details`.`advisorName` = p_advisor
+	AND   `dc_advisor_details`.`repId` = p_rep;
+
+    IF (tTemplateID is null)
+    THEN
+		SELECT "Not sufficient data to create new ID.  Either Template ID has to be provided or can only update data.";
+	ELSE
+		IF (tFunction = 'U')
+			THEN
+				UPDATE `invdb`.`dc_advisor_details`
+				SET`advisorCode` = p_advisorCode,
+					`firmName` = p_firmname,
+					`primaryContact` = p_primaryContact,
+					`email` = p_ops_email,
+					`updated` = now(),
+					`updatedBy` = 'SYSTEM'
+				WHERE `dc_advisor_details`.`advisorName` = p_advisor
+				AND   `dc_advisor_details`.`repId` = p_rep;
+			ELSE
+ 				IF (p_advisor is null and p_rep is null)
+					THEN
+						SELECT "Not sufficient data to create new ID.  Either Advisor and/or name is missing.";
+                    ELSE
+					INSERT INTO `invdb`.`dc_advisor_details`
+					(`advisorCode`,
+					`advisorName`,
+					`repId`,
+					`firmName`,
+					`primaryContact`,
+					`email`,
+					`created`,
+					`createdBy`)
+					SELECT 
+						IFNULL(p_advisorCode, `dc_advisor_details`.`advisorCode`),
+						p_advisor,
+						p_rep,
+						IFNULL(p_firmname, `dc_advisor_details`.`firmName`),
+						IFNULL(p_primaryContact, `dc_advisor_details`.`primaryContact`),
+						IFNULL(p_ops_email, `dc_advisor_details`.`email`),
+						now(),
+						'SYSTEM'
+					FROM `invdb`.`dc_advisor_details`
+					WHERE `dc_advisor_details`.`id` = tTemplateID
+					;
+					
+					SELECT LAST_INSERT_ID() into tNewID;
+					
+					 INSERT INTO `invdb`.`adv_request_document_mappings`
+						(`advisorid`,
+						`action`,
+						`subaction`,
+						`reqType`,
+						`envelopeHeading`,
+						`seqno`,
+						`formType`)
+					SELECT tNewID,
+						`adv_request_document_mappings`.`action`,
+						`adv_request_document_mappings`.`subaction`,
+						`adv_request_document_mappings`.`reqType`,
+						`adv_request_document_mappings`.`envelopeHeading`,
+						`adv_request_document_mappings`.`seqno`,
+						`adv_request_document_mappings`.`formType`
+					FROM `invdb`.`adv_request_document_mappings`
+					WHERE `adv_request_document_mappings`.`advisorid` = tTemplateID;				
+				END IF;
+		END IF;
+        
+		
+    END IF;
+    
+    
+END$$
+DELIMITER ;
