1d076670f82f7be05eaa0ac93935bfe8e5c0fc74
sagar <sagar@bd0249eb-a025-43e3-b6fa-fed758350ba6>
2017-04-19 12:54:27 +0000

    DBScript Changes by sagar for Pricing module

--- Invessence/DBScripts/Release_20170429-UOB/Pricing/13.sp_create_proc_rbsa.exchange_rate_processing.sql
+++ Invessence/DBScripts/Release_20170429-UOB/Pricing/13.sp_create_proc_rbsa.exchange_rate_processing.sql
@@ -14,20 +14,21 @@ begin
  select currency_exchange_cd into p_exchngsymbol from invdb.sec_source_mapping where ticker=p_ticker;
  
  select count(*),fromCurrency,toCurrency into p_exchngavailable,p_fromCurrency,p_toCurrency from invdb.sec_exchangerate_master where symbol=p_exchngsymbol;
- 
- if(p_exchngavailable>0) then 
- select p_exchngsymbol,p_exchngavailable,'1';
+
+ if(p_exchngavailable>0) then
+ select p_exchngsymbol,p_exchngavailable,'1',p_toCurrency;
  	 call rbsa.exchange_rate_processor(p_ticker,'forward',p_exchngsymbol,p_toCurrency);
-     else 
-     
+     else
+
  	set p_exchngavailable=0;
      SELECT SUBSTRING(p_exchngsymbol,1,3) into p_frstcd;
-     SELECT SUBSTRING(p_exchngsymbol,4,6) into p_scndcd;    
+     SELECT SUBSTRING(p_exchngsymbol,4,6) into p_scndcd;
      set p_exchngsymbol=concat(p_scndcd,p_frstcd);
  	select count(*) into p_exchngavailable from invdb.sec_exchangerate_master where symbol=p_exchngsymbol;
-     
- 		if(p_exchngavailable>0) then 
- 			select '2',p_exchngavailable,p_exchngsymbol;
+
+ 		if(p_exchngavailable>0) then
+            set p_fromCurrency=p_scndcd;
+ 			select '2',p_exchngavailable,p_exchngsymbol,p_fromCurrency;
  	 call rbsa.exchange_rate_processor(p_ticker,'backward',p_exchngsymbol,p_fromCurrency);
  		end if;
  end if;
