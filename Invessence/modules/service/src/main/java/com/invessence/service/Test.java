cd429c8acbbcd6b7a7ad2f3446a78a1ef4f3a653
abhangp <abhangp@bd0249eb-a025-43e3-b6fa-fed758350ba6>
2017-05-08 06:01:29 +0000

    Abhang checked in 05/08/2017 SVN testing

--- Invessence/modules/service/src/main/java/com/invessence/service/Test.java
+++ Invessence/modules/service/src/main/java/com/invessence/service/Test.java
@@ -1,5 +1,6 @@
 package com.invessence.service;
 
+import com.invessence.service.dao.ServiceDao;
 import com.invessence.service.util.*;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -15,9 +16,10 @@ public class Test
       {
 
          ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("serviceConfig.xml");
-         TestImpl serviceLayer = (TestImpl) context.getBean("testImpl");
-         System.out.println(ServiceParameters.getServiceProvider(Constant.SERVICES.EMAIL_SERVICE.toString()));
-         System.out.println(ServiceParameters.getConfigProperty(Constant.SERVICES.EMAIL_SERVICE.toString(),ServiceParameters.getServiceProvider(Constant.SERVICES.EMAIL_SERVICE.toString()),"HOST"));
+         ServiceDao wsCommonDao = (ServiceDao) context.getBean("wsCommonDao");
+         new  ServiceDetails(wsCommonDao);
+//         System.out.println(ServiceParameters.getServiceProvider(Constant.SERVICES.EMAIL_SERVICE.toString()));
+//         System.out.println(ServiceParameters.getConfigProperty(Constant.SERVICES.EMAIL_SERVICE.toString(),ServiceParameters.getServiceProvider(Constant.SERVICES.EMAIL_SERVICE.toString()),"HOST"));
    }
       catch (Exception e)
       {
