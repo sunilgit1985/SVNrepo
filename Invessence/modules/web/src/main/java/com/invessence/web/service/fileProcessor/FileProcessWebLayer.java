e126e6accf61ee99330f3a51fba33842dd5a6293
abhangp <abhangp@bd0249eb-a025-43e3-b6fa-fed758350ba6>
2017-05-19 11:25:06 +0000

    Abhang checked in 05/19/2017 File Processor Code

---
+++ Invessence/modules/web/src/main/java/com/invessence/web/service/fileProcessor/FileProcessWebLayer.java
@@ -0,0 +1,11 @@
+package com.invessence.web.service.fileProcessor;
+
+import com.invessence.service.bean.ServiceRequest;
+
+/**
+ * Created by abhangp on 5/19/2017.
+ */
+public interface FileProcessWebLayer
+{
+   public boolean processFile(ServiceRequest serviceRequest);
+}
