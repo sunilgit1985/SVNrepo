78aa77f076d317d845cca568418026603afb778b
abhangp <abhangp@bd0249eb-a025-43e3-b6fa-fed758350ba6>
2017-05-19 13:28:20 +0000

    Abhang checked in 05/19/2017 File Processor Code

--- Invessence/modules/web/src/main/java/com/invessence/web/bean/consumer/FileProcessorBean.java
+++ Invessence/modules/web/src/main/java/com/invessence/web/bean/consumer/FileProcessorBean.java
@@ -1,5 +1,6 @@
 package com.invessence.web.bean.consumer;
 
+import java.io.*;
 import javax.faces.bean.*;
 import javax.faces.context.FacesContext;
 
@@ -9,6 +10,7 @@ import com.invessence.web.service.aggr.*;
 import com.invessence.web.service.fileProcessor.FileProcessWebLayer;
 import com.invessence.web.util.*;
 import org.primefaces.context.RequestContext;
+import org.primefaces.model.*;
 
 /**
  * Created by abhangp on 05/19/2017.
@@ -67,12 +69,12 @@ public class FileProcessorBean
       this.logonid = logonid;
    }
 
-
-   public void processDownloadFile(){
+// Need to return the list of file
+   public void processDownloadFile(String processId){
       System.out.println("AggregationBean.startup");
-      String product = getWebutil().getWebprofile().getWebInfo().get("SERVICE.CUSTODY").toString();
+      String product = getWebutil().getWebprofile().getWebInfo().get("SERVICE.PRODUCT").toString();
       String serviceMode = getWebutil().getWebprofile().getWebInfo().get("SERVICE.FILEPROCESS.MODE").toString();
-      String processId= getWebutil().getWebprofile().getWebInfo().get("SERVICE.FILEPROCESS.UPLOADPROCESSID").toString();
+//      String processId= getWebutil().getWebprofile().getWebInfo().get("SERVICE.FILEPROCESS.UPLOADPROCESSID").toString();
       System.out.println("Product " + product);
       System.out.println("ServiceMode " + serviceMode);
       System.out.println("ProcessId = " + processId);
@@ -81,7 +83,7 @@ public class FileProcessorBean
             logonid = webutil.getLogonid();
          }
 
-         boolean  result=fileProcessWebLayer.processFile(new ServiceRequest("UOB","UAT","UPLD1"));
+         boolean  result=fileProcessWebLayer.processFile(new ServiceRequest(product, serviceMode, processId));
          System.out.println("result = " + result);
 
 //         if(result==true){
@@ -97,4 +99,26 @@ public class FileProcessorBean
       }
    }
 
+
+   public StreamedContent downloadFile(String processId) {
+      StreamedContent file = null;
+      try {
+         InputStream stream = new FileInputStream(new File(processId))
+         {
+            @Override
+            public int read() throws IOException
+            {
+               return 0;
+            }
+         };
+         stream.close();
+         file = new DefaultStreamedContent(stream, "application/csv", processId);
+         // application/zip for zip file
+         return file;
+      }
+      catch (Exception ex) {
+
+      }
+      return file;
+   }
 }
