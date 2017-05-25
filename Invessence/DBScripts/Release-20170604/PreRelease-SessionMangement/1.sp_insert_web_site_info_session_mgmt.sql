5478a1b59797fd8deeb295a1c754b9106359e162
sagar <sagar@bd0249eb-a025-43e3-b6fa-fed758350ba6>
2017-05-25 15:09:41 +0000

    code check in by Sagar

--- Invessence/DBScripts/Release-20170604/PreRelease-SessionMangement/1.sp_insert_web_site_info_session_mgmt.sql
+++ Invessence/DBScripts/Release-20170604/PreRelease-SessionMangement/1.sp_insert_web_site_info_session_mgmt.sql
@@ -11,3 +11,8 @@ delete from invdb.web_site_info where name='WEB.SESSION.TIMEOUT';
 INSERT INTO invdb.web_site_info (url, name, status, value, encrFlag, created)
 select url,'WEB.SESSION.TIMEOUT','A','15','N',now() from invdb.web_site_info group by url;
 
+## Insertion script for mobile logout redirction
+delete from invdb.web_site_info where name='URL.MOBILE';
+
+INSERT INTO invdb.web_site_info (url, name, status, value, encrFlag, created)
+select url,'URL.MOBILE','A','','N',now() from invdb.web_site_info group by url;
