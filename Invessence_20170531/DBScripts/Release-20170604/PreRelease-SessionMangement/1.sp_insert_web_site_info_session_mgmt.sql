54db227845e616a6265020563002a49ac51b5f0c
sagar <sagar@bd0249eb-a025-43e3-b6fa-fed758350ba6>
2017-05-26 15:05:04 +0000

    sagar db changes

--- Invessence_20170531/DBScripts/Release-20170604/PreRelease-SessionMangement/1.sp_insert_web_site_info_session_mgmt.sql
+++ Invessence_20170531/DBScripts/Release-20170604/PreRelease-SessionMangement/1.sp_insert_web_site_info_session_mgmt.sql
@@ -1,5 +1,7 @@
 ## Insertion script for Session Countdown Time
 
+SET SQL_SAFE_UPDATES = 0;
+
 delete from invdb.web_site_info where name='WEB.SESSION.COUNTDOWNTIME';
 
 INSERT INTO invdb.web_site_info (url, name, status, value, encrFlag, created)
@@ -14,5 +16,16 @@ select url,'WEB.SESSION.TIMEOUT','A','15','N',now() from invdb.web_site_info gro
 ## Insertion script for mobile logout redirction
 delete from invdb.web_site_info where name='URL.MOBILE';
 
-INSERT INTO invdb.web_site_info (url, name, status, value, encrFlag, created)
-select url,'URL.MOBILE','A','','N',now() from invdb.web_site_info group by url;
+INSERT INTO invdb.web_site_info (url, name, status, value, encrFlag, created)values('buildingbenjamins','URL.MOBILE','A','','N',now());
+INSERT INTO invdb.web_site_info (url, name, status, value, encrFlag, created)values('traditionadvisers','URL.MOBILE','A','','N',now());
+INSERT INTO invdb.web_site_info (url, name, status, value, encrFlag, created)values('uatbb','URL.MOBILE','A','','N',now());
+INSERT INTO invdb.web_site_info (url, name, status, value, encrFlag, created)values('uattcm','URL.MOBILE','A','','N',now());
+
+SET SQL_SAFE_UPDATES = 1;
+
+
+
+
+
+
+
