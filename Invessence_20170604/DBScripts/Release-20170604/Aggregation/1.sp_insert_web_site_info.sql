cc0538e998d095ea154546a773a46986a00b67f0
sagar <sagar@bd0249eb-a025-43e3-b6fa-fed758350ba6>
2017-06-01 08:37:46 +0000

    by Sagar for db script

--- Invessence_20170604/DBScripts/Release-20170604/Aggregation/1.sp_insert_web_site_info.sql
+++ Invessence_20170604/DBScripts/Release-20170604/Aggregation/1.sp_insert_web_site_info.sql
@@ -3,4 +3,4 @@
 delete from invdb.web_site_info where name='MENU.AGGREGATOR';
 
 INSERT INTO invdb.web_site_info (url, name, status, value, encrFlag, created)
-select url,'MENU.AGGREGATOR','A','A','N',now() from invdb.web_site_info group by url;
+select url,'MENU.AGGREGATOR','A','N','N',now() from invdb.web_site_info group by url;
