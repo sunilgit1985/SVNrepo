4555e579babf2d48a7ae8644b6b1d929be3dd416
abhangp <abhangp@bd0249eb-a025-43e3-b6fa-fed758350ba6>
2017-06-09 12:32:13 +0000

    Abhang checked in 06/08/2017 CITI Demo

---
+++ Invessence/DBScripts/CitiDemo-20170609/web_sit_properties.sql
@@ -0,0 +1,4 @@
+use `invdb`;
+
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`) VALUES ('localhost:8090', 'WATSON.API.URL', 'A', 'http://192.168.100.122:8080/SocialNetwork/', 'N', '2017-05-15 19:56:55');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`) VALUES ('localhost:8090', 'LINKEDIN.CALLBACK.URL', 'A', 'http://localhost:8090/pages/consumer/citi/demo/demo.xhtml', 'N', '2017-05-15 19:56:55');
