d6eda068468c69ba2cc407926563fc7a4239d9c1
abhangp <abhangp@bd0249eb-a025-43e3-b6fa-fed758350ba6>
2017-06-22 12:10:54 +0000

    Abhang checked in 06/22/2017

---
+++ Invessence/DBScripts/SETUP/Master/1.build_uob_web_site.sql
@@ -0,0 +1,108 @@
+DELETE FROM  `invdb`.`web_site_info` where url = 'master';
+
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'ARCHIVE.CLOSED', 'A', '10', 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'ARCHIVE.INACTIVE', 'A', '30', 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'ARCHIVE.UNOPENED', 'A', '30', 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'CHART.ASSET.ALLOCATION', 'A', 'HIGHCHART.2DDONUT', 'N', '2017-02-21 20:02:19', null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'CHART.RECOMMENDED.ASSET.ALLOCATION', 'A', 'PRIMEFACES.BARCHART', 'N', '2017-02-21 20:02:19', null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'CSS.CUSTODY', 'A', 'css/invessenceCustom.css', 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'CSS.CUSTOM', 'A', 'css/invessenceCustom.css', 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'CSS.DIR', 'A', 'invessence', 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'CUSTODY.SERVICE', 'A', 'URL', 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'CUSTODY.URL', 'A', 'https://sguat.uobkayhian.com/robo/uwealth_signup.jsp', 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'DEFAULT.ADVISOR', 'A', 'uob', 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'DEFAULT.MODEL', 'A', '0.SGWealth', 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'DEFAULT.REP', 'A', '', 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'DIR.CONSUMER', 'A', 'uob', 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'DIR.CUSTODY', 'A', null, 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'DIR.TEMPLATE', 'A', '/template/common/invessence', 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'EMAIL.MAIN', 'A', 'info@uobkh.com', 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'EMAIL.OPERATION', 'A', 'operations@uobkh.com', 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'EMAIL.SUPPORT', 'A', 'support@uobkh.com', 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'EMAIL.USER', 'A', 'noreply@uobkh.com', 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'HTML.FORGOT', 'A', 'UOBKH-Forgot.htm', 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'HTML.LOCKED', 'A', 'UOBKH-Locked.htm', 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'HTML.RESET', 'A', 'UOBKH-Reset.htm', 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'HTML.WELCOME', 'A', 'UOBKH-Welcome.htm', 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'HTML.WELCOME.ADV', 'A', 'UOBKH-Welcome.htm', 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'INVESTMENT.MIN1ST', 'A', '2000', 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'INVESTMENT.MIN2ND', 'A', '50', 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'INVESTMENT.RECURRING1ST', 'A', '50', 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'INVESTMENT.RECURRING2ND', 'A', '50', 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'MENU.AGGREGRATION', 'A', 'User', 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'PHONE.MAIN', 'A', '(XXX) XXX-XXXX', 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'PHONE.SUPPORT', 'A', '(XXX) XXX-XXXX', 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'PRODUCT.NAME', 'A', 'UOBKH', 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'SUBJECT.EMAIL.ACTIVATE', 'A', 'Welcome to UOB Kay Hian– Activate Your Account', 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'SUBJECT.EMAIL.LOCKED', 'A', 'UOB Kay Hian - Account is locked', 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'SUBJECT.EMAIL.RESET', 'A', 'UOB Kay Hian– Reset Your Password', 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'SUBJECT.EMAIL.WELCOME', 'A', 'Welcome to UOB Kay Hian– Activate Your Account', 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'URL.SECURE', 'A', 'https://www.uwealth.com.sg', 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'URL.WEBSITE', 'A', 'https://www.uwealth.com.sg', 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'WEB.COMPANYNAME', 'A', 'UOB Kay Hian', 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'WEB.COPYRIGHT', 'A', 'UOB Kay Hian', 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'WEB.FAVICONLOGO', 'A', '/javax.faces.resource/images/Utradefavicon.ico.xhtml?ln=uob', null, null, null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'WEB.GOOGLEANALYTICS', 'A', 'inv_googleanalytics.js', 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'WEB.LOGO', 'A', 'images/logo/UtradeLogo.png', 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'WEB.LOGOLIB', 'A', 'invessence', 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'WEB.LOGOSIZE', 'A', '200px', 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'WEB.MODE', 'A', 'PROD', 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'WEB.THEME', 'A', 'invessence', 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'HTML.BASE.PATH', 'A', '/inv/www/invessence/ROOT/template/html/', 'N', now(), null);
+
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'SERVICE.CUSTODY', 'A', null, 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'SERVICE.DOCUSIGN.MODE', 'A', 'PROD', 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'SERVICE.EMAILER.MODE', 'A', 'PROD', 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'SERVICE.PRICING.MODE', 'A', 'PROD', 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'URL.MOBILE', 'A', 'https://www.uwealth.com.sg', 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'WEB.SSLSEAL', 'A', null, 'N', now(), null);
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('master', 'WEB.THEMELIB', 'A', 'invessence', 'N', now(), null);
+
+DELETE FROM `invdb`.`web_site_info` WHERE `web_site_info`.`url` = 'uob';
+INSERT INTO `invdb`.`web_site_info`
+SELECT 'uob',
+    `web_site_info`.`name`,
+    `web_site_info`.`status`,
+    `web_site_info`.`value`,
+    `web_site_info`.`encrFlag`,
+    `web_site_info`.`created`,
+    `web_site_info`.`updated`
+FROM `invdb`.`web_site_info`
+WHERE `web_site_info`.`url` = 'master';
+
+DELETE FROM `invdb`.`web_site_info` WHERE `web_site_info`.`url` = 'preuob';
+INSERT INTO `invdb`.`web_site_info`
+SELECT 'preuob',
+    `web_site_info`.`name`,
+    `web_site_info`.`status`,
+    `web_site_info`.`value`,
+    `web_site_info`.`encrFlag`,
+    `web_site_info`.`created`,
+    `web_site_info`.`updated`
+FROM `invdb`.`web_site_info`
+WHERE `web_site_info`.`url` = 'master';
+
+DELETE FROM `invdb`.`web_site_info` WHERE `web_site_info`.`url` = 'uatuob';
+INSERT INTO `invdb`.`web_site_info`
+SELECT 'uatuob',
+    `web_site_info`.`name`,
+    `web_site_info`.`status`,
+    `web_site_info`.`value`,
+    `web_site_info`.`encrFlag`,
+    `web_site_info`.`created`,
+    `web_site_info`.`updated`
+FROM `invdb`.`web_site_info`
+WHERE `web_site_info`.`url` = 'master';
+
+DELETE FROM `invdb`.`web_site_info` WHERE `web_site_info`.`url` = 'demouob';
+INSERT INTO `invdb`.`web_site_info`
+SELECT 'demouob',
+    `web_site_info`.`name`,
+    `web_site_info`.`status`,
+    `web_site_info`.`value`,
+    `web_site_info`.`encrFlag`,
+    `web_site_info`.`created`,
+    `web_site_info`.`updated`
+FROM `invdb`.`web_site_info`
+WHERE `web_site_info`.`url` = 'master';
+
