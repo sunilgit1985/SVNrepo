a2e091ad0b2a271acd9f59fb5837394d1fb86183
pmehta <pmehta@bd0249eb-a025-43e3-b6fa-fed758350ba6>
2017-06-23 19:48:48 +0000

    Prashant 06/23/2017 Web_site_info_managements

---
+++ Invessence/DBScripts/SETUP/Master/2b.build_tcm_web_site.sql
@@ -0,0 +1,66 @@
+DELETE FROM `invdb`.`web_site_info` where url = 'traditionadvisers';
+
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'ARCHIVE.CLOSED', 'A', '10', 'N', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'ARCHIVE.INACTIVE', 'A', '30', 'N', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'ARCHIVE.UNOPENED', 'A', '30', 'N', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'CSS.CUSTODY', 'A', 'css/tcm.css', 'N', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'CSS.CUSTOM', 'A', 'css/tcm.css', 'N', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'CSS.DIR', 'A', 'tcm', 'N', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'CUSTODY.SERVICE', 'A', 'INTERNAL', 'N', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'CUSTODY.URL', 'A', '', 'N', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'DEFAULT.ADVISOR', 'A', 'BB-TCM', 'N', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'DEFAULT.MODEL', 'A', '0.TA', 'N', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'DEFAULT.REP', 'A', '', 'N', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'DIR.CONSUMER', 'A', 'tcm', 'N', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'DIR.CUSTODY', 'A', 'td', 'N', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'DIR.TEMPLATE', 'A', '/template/common/spark', 'N', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'EMAIL.MAIN', 'A', 'info@traditionadvisers.net', 'N', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'EMAIL.OPERATION', 'A', 'operations@traditionadvisers.net', 'N', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'EMAIL.SUPPORT', 'A', 'support@traditionadvisers.net', 'N', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'EMAIL.USER', 'A', 'noreply@traditionadvisers.net', 'N', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'HTML.BASE.PATH', 'A', '/inv/www/invessence/ROOT/template/html/', 'N', '2017-04-10 00:00:00', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'HTML.FORGOT', 'A', 'traditionadvisers-Forgot.html', 'N', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'HTML.LOCKED', 'A', 'traditionadvisers-Locked.html', 'N', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'HTML.RESET', 'A', 'traditionadvisers-Reset.html', 'N', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'HTML.WELCOME', 'A', 'traditionadvisers-Welcome.html', 'N', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'HTML.WELCOME.ADV', 'A', 'traditionadvisers-Welcome.html', 'N', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'INVESTMENT.MIN1ST', 'A', '2000', 'N', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'INVESTMENT.MIN2ND', 'A', '50', 'N', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'INVESTMENT.RECURRING1ST', 'A', '50', 'N', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'INVESTMENT.RECURRING2ND', 'A', '50', 'N', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'MENU.AGGREGATOR', 'A', 'N', 'N', '2017-06-03 14:13:52', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'PHONE.MAIN', 'A', '(908) 333-4733', 'N', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'PHONE.SUPPORT', 'A', '(908) 333-4733', 'N', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'SERVICE.CUSTODY', 'A', 'TCM', 'N', '2017-04-08 19:44:52', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'SERVICE.DOCUSIGN.MODE', 'A', 'PROD', 'N', '2017-04-08 19:44:52', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'SERVICE.EMAILER.MODE', 'A', 'PROD', 'N', '2017-04-08 19:44:52', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'SERVICE.PRICING.MODE', 'A', 'PROD', 'N', '2017-04-08 19:44:52', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'SUBJECT.EMAIL.ACTIVATE', 'A', 'Welcome to Tradition Advisers - Activate Your Account', 'N', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'SUBJECT.EMAIL.LOCKED', 'A', 'Tradition Advisers - Account is locked', 'N', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'SUBJECT.EMAIL.RESET', 'A', 'Tradition Advisers - Reset Your Password', 'N', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'SUBJECT.EMAIL.WELCOME', 'A', 'Welcome to Tradition Advisers - Activate Your Account', 'N', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'URL.MOBILE', 'A', 'https://traditionadvisers.net/mobilelogin.xhtml', 'N', '2017-06-03 14:14:13', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'URL.SECURE', 'A', 'https://traditionadvisers.net', 'N', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'URL.WEBSITE', 'A', 'http://traditionadvisers.net', 'N', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'WEB.COMPANYNAME', 'A', 'Tradition Advisers', 'N', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'WEB.COPYRIGHT', 'A', 'Tradition Capital Management', 'N', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'WEB.FAVICONLOGO', 'A', '/javax.faces.resource/images/tcmicon.png.xhtml?ln=tcm', 'N', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'WEB.GOOGLEANALYTICS', 'A', 'bb_googleanalytics.js', 'N', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'WEB.LOGO', 'A', 'images/logo/tcm-logo.jpe', 'N', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'WEB.LOGOLIB', 'A', 'invessence', 'N', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'WEB.LOGOSIZE', 'A', '200px', 'N', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'WEB.MODE', 'A', 'PROD', 'N', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'WEB.SESSION.COUNTDOWNTIME', 'A', '2', 'N', '2017-06-03 14:14:13', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'WEB.SESSION.TIMEOUT', 'A', '15', 'N', '2017-06-03 14:14:13', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'WEB.SSLSEAL', 'A', 'https://seal.thawte.com/getthawteseal?host_name=www.buildingbenjamins.com&amp;size=S&amp;lang=en', 'N', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'WEB.THEME', 'A', 'spark', 'N', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('traditionadvisers', 'WEB.THEMELIB', 'A', 'spark-layout', 'N', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+
+-- Using master as source Insert any missing data from saved.
+INSERT INTO `invdb`.`web_site_info`
+(`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`)
+SELECT `url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`
+FROM `invdb`.`web_site_info`
+where url = 'master'
+AND `name` not in (select `name` from `invdb`.`web_site_info` where url = 'traditionadvisers');
+
---
+++ Invessence/DBScripts/SETUP/Master/3a.build_uob_web_site.sql
@@ -0,0 +1,70 @@
+DELETE FROM `invdb`.`web_site_info` where url = 'uwealth';
+
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'ARCHIVE.CLOSED', 'A', '10', 'N', '2017-06-03 13:31:07', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'ARCHIVE.INACTIVE', 'A', '30', 'N', '2017-06-03 13:31:07', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'ARCHIVE.UNOPENED', 'A', '30', 'N', '2017-06-03 13:31:07', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'CHART.ASSET.ALLOCATION', 'A', 'HIGHCHART.2DDONUT', 'N', '2017-02-21 20:02:19', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'CHART.RECOMMENDED.ASSET.ALLOCATION', 'A', 'PRIMEFACES.BARCHART', 'N', '2017-02-21 20:02:19', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'CSS.CUSTODY', 'A', 'css/invessenceCustom.css', 'N', '2017-06-03 13:31:07', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'CSS.CUSTOM', 'A', 'css/invessenceCustom.css', 'N', '2017-06-03 13:31:07', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'CSS.DIR', 'A', 'invessence', 'N', '2017-06-03 13:31:07', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'CUSTODY.SERVICE', 'A', 'URL', 'N', '2017-06-03 13:31:07', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'CUSTODY.URL', 'A', 'https://sguat.uobkayhian.com/robo/uwealth_signup.jsp', 'N', '2017-06-03 13:31:07', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'DEFAULT.ADVISOR', 'A', 'uob', 'N', '2017-06-03 13:31:07', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'DEFAULT.MODEL', 'A', '0.SGWealth', 'N', '2017-06-03 13:31:07', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'DEFAULT.REP', 'A', '', 'N', '2017-06-03 13:31:07', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'DIR.CONSUMER', 'A', 'uob', 'N', '2017-06-03 13:31:07', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'DIR.CUSTODY', 'A', 'NULL', 'N', '2017-06-03 13:31:07', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'DIR.TEMPLATE', 'A', '/template/common/invessence', 'N', '2017-06-03 13:31:07', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'EMAIL.MAIN', 'A', 'info@uobkh.com', 'N', '2017-06-03 13:31:07', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'EMAIL.OPERATION', 'A', 'operations@uobkh.com', 'N', '2017-06-03 13:31:07', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'EMAIL.SUPPORT', 'A', 'support@uobkh.com', 'N', '2017-06-03 13:31:07', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'EMAIL.USER', 'A', 'noreply@uobkh.com', 'N', '2017-06-03 13:31:07', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'HTML.BASE.PATH', 'A', '/inv/www/invessence/ROOT/template/html/', 'N', '2017-06-03 13:31:07', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'HTML.FORGOT', 'A', 'UOBKH-Forgot.htm', 'N', '2017-06-03 13:31:07', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'HTML.LOCKED', 'A', 'UOBKH-Locked.htm', 'N', '2017-06-03 13:31:07', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'HTML.RESET', 'A', 'UOBKH-Reset.htm', 'N', '2017-06-03 13:31:07', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'HTML.WELCOME', 'A', 'UOBKH-Welcome.htm', 'N', '2017-06-03 13:31:07', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'HTML.WELCOME.ADV', 'A', 'UOBKH-Welcome.htm', 'N', '2017-06-03 13:31:07', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'INVESTMENT.MIN1ST', 'A', '2000', 'N', '2017-06-03 13:31:07', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'INVESTMENT.MIN2ND', 'A', '50', 'N', '2017-06-03 13:31:07', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'INVESTMENT.RECURRING1ST', 'A', '50', 'N', '2017-06-03 13:31:07', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'INVESTMENT.RECURRING2ND', 'A', '50', 'N', '2017-06-03 13:31:07', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'MENU.AGGREGATOR', 'A', 'N', 'N', '2017-06-03 14:13:52', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'MENU.AGGREGRATION', 'A', 'User', 'N', '2017-06-03 13:31:07', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'PHONE.MAIN', 'A', '(XXX) XXX-XXXX', 'N', '2017-06-03 13:31:07', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'PHONE.SUPPORT', 'A', '(XXX) XXX-XXXX', 'N', '2017-06-03 13:31:07', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'PRODUCT.NAME', 'A', 'UOBKH', 'N', '2017-06-03 13:31:07', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'SERVICE.CUSTODY', 'A', 'NULL', 'N', '2017-06-03 13:31:07', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'SERVICE.DOCUSIGN.MODE', 'A', 'PROD', 'N', '2017-06-03 13:31:07', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'SERVICE.EMAILER.MODE', 'A', 'PROD', 'N', '2017-06-03 13:31:07', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'SERVICE.PRICING.MODE', 'A', 'PROD', 'N', '2017-06-03 13:31:07', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'SUBJECT.EMAIL.ACTIVATE', 'A', 'Welcome to UOB Kay Hian– Activate Your Account', 'N', '2017-06-03 13:31:07', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'SUBJECT.EMAIL.LOCKED', 'A', 'UOB Kay Hian - Account is locked', 'N', '2017-06-03 13:31:07', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'SUBJECT.EMAIL.RESET', 'A', 'UOB Kay Hian– Reset Your Password', 'N', '2017-06-03 13:31:07', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'SUBJECT.EMAIL.WELCOME', 'A', 'Welcome to UOB Kay Hian– Activate Your Account', 'N', '2017-06-03 13:31:07', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'URL.MOBILE', 'A', 'https://www.uwealth.com/mobile.xhtml', 'N', '2017-06-03 14:14:13', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'URL.SECURE', 'A', 'https://www.uwealth.com.sg', 'N', '2017-06-03 13:31:07', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'URL.WEBSITE', 'A', 'https://www.uwealth.com.sg', 'N', '2017-06-03 13:31:07', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'WEB.COMPANYNAME', 'A', 'UOB Kay Hian', 'N', '2017-06-03 13:31:07', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'WEB.COPYRIGHT', 'A', 'UOB Kay Hian', 'N', '2017-06-03 13:31:07', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'WEB.FAVICONLOGO', 'A', '/javax.faces.resource/images/Utradefavicon.ico.xhtml?ln=uob', 'NULL', 'NULL', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'WEB.GOOGLEANALYTICS', 'A', 'inv_googleanalytics.js', 'N', '2017-06-03 13:31:07', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'WEB.LOGO', 'A', 'images/logo/UtradeLogo.png', 'N', '2017-06-03 13:31:07', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'WEB.LOGOLIB', 'A', 'invessence', 'N', '2017-06-03 13:31:07', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'WEB.LOGOSIZE', 'A', '200px', 'N', '2017-06-03 13:31:07', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'WEB.MODE', 'A', 'PROD', 'N', '2017-06-03 13:31:07', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'WEB.SESSION.COUNTDOWNTIME', 'A', '2', 'N', '2017-06-03 14:14:13', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'WEB.SESSION.TIMEOUT', 'A', '15', 'N', '2017-06-03 14:14:13', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'WEB.SSLSEAL', 'A', 'NULL', 'N', '2017-06-03 13:31:07', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'WEB.THEME', 'A', 'invessence', 'N', '2017-06-03 13:31:07', 'NULL');
+INSERT INTO `invdb`.`web_site_info` (`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`) VALUES ('uwealth', 'WEB.THEMELIB', 'A', 'invessence', 'N', '2017-06-03 13:31:07', 'NULL');
+
+-- Using master as source Insert any missing data from saved.
+INSERT INTO `invdb`.`web_site_info`
+(`url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`)
+SELECT `url`, `name`, `status`, `value`, `encrFlag`, `created`, `updated`
+FROM `invdb`.`web_site_info`
+where url = 'master'
+AND `name` not in (select `name` from `invdb`.`web_site_info` where url = 'uwealth');
+
