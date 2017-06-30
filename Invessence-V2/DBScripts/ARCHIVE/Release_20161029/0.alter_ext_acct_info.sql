debee8c45ccc93404bdfdfda3ab41e231997dd3e
sagar <sagar@bd0249eb-a025-43e3-b6fa-fed758350ba6>
2017-06-30 09:44:09 +0000

    code copied by sagar

--- Invessence-V2/DBScripts/ARCHIVE/Release_20161029/0.alter_ext_acct_info.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `invdb`.`ext_acct_info` 
-ADD COLUMN `acctType` VARCHAR(40) NULL DEFAULT NULL COMMENT '' AFTER `dob`;
--- Invessence-V2/DBScripts/Release_20170429-UOB/Download/4.sp_alter_invdb_ext_position.sql
+++
@@ -1,6 +0,0 @@
-## Script for altering invdb.ext_position
-
-ALTER TABLE invdb.ext_position
-CHANGE COLUMN levelOfDetail levelOfDetail VARCHAR(40) NOT NULL DEFAULT 0 ,
-DROP PRIMARY KEY,
-ADD PRIMARY KEY (clientAccountID, reportDate, symbol, purchaseDate, costBasisPrice, levelOfDetail);
