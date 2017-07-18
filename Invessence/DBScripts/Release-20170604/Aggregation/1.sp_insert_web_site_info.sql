5733b9a0b375eeb82f0d4567cbac93fc9c92b253
sagar <sagar@bd0249eb-a025-43e3-b6fa-fed758350ba6>
2017-07-18 14:18:00 +0000

    Code check in By Sagar

--- Invessence/DBScripts/Release-20170604/Aggregation/1.sp_insert_web_site_info.sql
+++ Invessence/DBScripts/Release-20170604/Aggregation/1.sp_insert_web_site_info.sql
@@ -1,6 +1,10 @@
 ## Insertion script for Aggregator Menu
 
-delete from invdb.web_site_info where name='MENU.AGGREGATOR';
+SET SQL_SAFE_UPDATES = 0;
+
+delete from invdb.web_site_info where name=''MENU.AGGREGATOR'';
 
 INSERT INTO invdb.web_site_info (url, name, status, value, encrFlag, created)
-select url,'MENU.AGGREGATOR','A','A','N',now() from invdb.web_site_info group by url;
+select url,''MENU.AGGREGATOR'',''A'',''N'',''N'',now() from invdb.web_site_info group by url;
+
+SET SQL_SAFE_UPDATES = 1;
