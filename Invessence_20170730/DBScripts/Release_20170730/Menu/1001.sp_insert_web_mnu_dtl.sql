a3a363b82360c04e83111cafff09f265c786241d
sagar <sagar@bd0249eb-a025-43e3-b6fa-fed758350ba6>
2017-07-24 14:53:14 +0000

    commit by sagar for issue fixing removal of unwanted link removal

--- Invessence_20170730/DBScripts/Release_20170730/Menu/1001.sp_insert_web_mnu_dtl.sql
+++ Invessence_20170730/DBScripts/Release_20170730/Menu/1001.sp_insert_web_mnu_dtl.sql
@@ -6,7 +6,6 @@ delete from invdb.web_menu where url<>'master';
 
 INSERT INTO invdb.web_menu (url, access, permission, label, level, sublevel, seq, status, availOnMobile, destdir, htmlpage, created) VALUES ('pretcm.invessence.com', 'Advisor', 'Admin', 'Login Management', '0', '0', '8', 'N', 'N', 'advisor', 'user_mng.xhtml',now());
 INSERT INTO invdb.web_menu (url, access, permission, label, level, sublevel, seq, status, availOnMobile, destdir, htmlpage, created) VALUES ('pretcm.invessence.com', 'Advisor', 'Admin', 'Email Logs', '0', '0', '10', 'N', 'N', 'productadmin', 'email_logs.xhtml', now());
-INSERT INTO invdb.web_menu (url, access, permission, label, level, sublevel, seq, status, availOnMobile, destdir, htmlpage, created) VALUES ('pretcm.invessence.com', 'Advisor', 'Admin', 'Login Management', '0', '0', '8', 'N', 'N', 'advisor', 'user_mng.xhtml', now());
 INSERT INTO invdb.web_menu (url, access, permission, label, level, sublevel, seq, status, availOnMobile, destdir, htmlpage, created) VALUES ('pretcm.invessence.com', 'Advisor', 'Operations', 'Model', '0', '0', '7', 'A', 'N', 'advisor', 'assetworkflow.xhtml',now());
 INSERT INTO invdb.web_menu (url, access, permission, label, level, sublevel, seq, status, availOnMobile, destdir, htmlpage, created) VALUES ('pretcm.invessence.com', 'Advisor', 'Operations', 'Trade', '0', '0', '3', 'A', 'N', 'advisor/operations', 'trade.xhtml',now());
 INSERT INTO invdb.web_menu (url, access, permission, label, level, sublevel, seq, status, availOnMobile, destdir, htmlpage, created) VALUES ('pretcm.invessence.com', 'SuperAdmin', 'ProductAdmin', 'Account Emulation', '0', '0', '12', 'A', 'N', 'productadmin', 'accountEmulation.xhtml',now());
@@ -15,7 +14,6 @@ INSERT INTO invdb.web_menu (url, access, permission, label, level, sublevel, seq
 INSERT INTO invdb.web_menu (url, access, permission, label, level, sublevel, seq, status, availOnMobile, destdir, htmlpage, created) VALUES ('pretcm.invessence.com', 'User', 'User', 'Aggregation', '0', '0', '11', 'N', 'N', '', '',now());
 INSERT INTO invdb.web_menu (url, access, permission, label, level, sublevel, seq, status, availOnMobile, destdir, htmlpage, created) VALUES ('pretcm.invessence.com', 'User', 'User', 'Reports', '0', '0', '11', 'N', 'N', 'consumer', 'creports.xhtml',now());
 
-INSERT INTO invdb.web_menu (url, access, permission, label, level, sublevel, seq, status, availOnMobile, destdir, htmlpage, created) VALUES ('uattcm.invessence.com', 'Advisor', 'Admin', 'Login Management', '0', '0', '8', 'N', 'N', 'advisor', 'user_mng.xhtml',now());
 INSERT INTO invdb.web_menu (url, access, permission, label, level, sublevel, seq, status, availOnMobile, destdir, htmlpage, created) VALUES ('uattcm.invessence.com', 'Advisor', 'Admin', 'Email Logs', '0', '0', '10', 'N', 'N', 'productadmin', 'email_logs.xhtml', now());
 INSERT INTO invdb.web_menu (url, access, permission, label, level, sublevel, seq, status, availOnMobile, destdir, htmlpage, created) VALUES ('uattcm.invessence.com', 'Advisor', 'Admin', 'Login Management', '0', '0', '8', 'N', 'N', 'advisor', 'user_mng.xhtml', now());
 INSERT INTO invdb.web_menu (url, access, permission, label, level, sublevel, seq, status, availOnMobile, destdir, htmlpage, created) VALUES ('uattcm.invessence.com', 'Advisor', 'Operations', 'Model', '0', '0', '7', 'A', 'N', 'advisor', 'assetworkflow.xhtml',now());
@@ -27,7 +25,7 @@ INSERT INTO invdb.web_menu (url, access, permission, label, level, sublevel, seq
 INSERT INTO invdb.web_menu (url, access, permission, label, level, sublevel, seq, status, availOnMobile, destdir, htmlpage, created) VALUES ('uattcm.invessence.com', 'User', 'User', 'Reports', '0', '0', '11', 'N', 'N', 'consumer', 'creports.xhtml',now());
 
 
-INSERT INTO invdb.web_menu (url, access, permission, label, level, sublevel, seq, status, availOnMobile, destdir, htmlpage, created) VALUES ('prebb.invessence.com', 'Advisor', 'Admin', 'Login Management', '0', '0', '8', 'N', 'N', 'advisor', 'user_mng.xhtml',now());
+
 INSERT INTO invdb.web_menu (url, access, permission, label, level, sublevel, seq, status, availOnMobile, destdir, htmlpage, created) VALUES ('prebb.invessence.com', 'Advisor', 'Admin', 'Email Logs', '0', '0', '10', 'N', 'N', 'productadmin', 'email_logs.xhtml', now());
 INSERT INTO invdb.web_menu (url, access, permission, label, level, sublevel, seq, status, availOnMobile, destdir, htmlpage, created) VALUES ('prebb.invessence.com', 'Advisor', 'Admin', 'Login Management', '0', '0', '8', 'N', 'N', 'advisor', 'user_mng.xhtml', now());
 INSERT INTO invdb.web_menu (url, access, permission, label, level, sublevel, seq, status, availOnMobile, destdir, htmlpage, created) VALUES ('prebb.invessence.com', 'Advisor', 'Operations', 'Model', '0', '0', '7', 'A', 'N', 'advisor', 'assetworkflow.xhtml',now());
@@ -38,7 +36,7 @@ INSERT INTO invdb.web_menu (url, access, permission, label, level, sublevel, seq
 INSERT INTO invdb.web_menu (url, access, permission, label, level, sublevel, seq, status, availOnMobile, destdir, htmlpage, created) VALUES ('prebb.invessence.com', 'User', 'User', 'Aggregation', '0', '0', '11', 'N', 'N', '', '',now());
 INSERT INTO invdb.web_menu (url, access, permission, label, level, sublevel, seq, status, availOnMobile, destdir, htmlpage, created) VALUES ('prebb.invessence.com', 'User', 'User', 'Reports', '0', '0', '11', 'N', 'N', 'consumer', 'creports.xhtml',now());
 
-INSERT INTO invdb.web_menu (url, access, permission, label, level, sublevel, seq, status, availOnMobile, destdir, htmlpage, created) VALUES ('uatbb.invessence.com', 'Advisor', 'Admin', 'Login Management', '0', '0', '8', 'N', 'N', 'advisor', 'user_mng.xhtml',now());
+
 INSERT INTO invdb.web_menu (url, access, permission, label, level, sublevel, seq, status, availOnMobile, destdir, htmlpage, created) VALUES ('uatbb.invessence.com', 'Advisor', 'Admin', 'Email Logs', '0', '0', '10', 'N', 'N', 'productadmin', 'email_logs.xhtml', now());
 INSERT INTO invdb.web_menu (url, access, permission, label, level, sublevel, seq, status, availOnMobile, destdir, htmlpage, created) VALUES ('uatbb.invessence.com', 'Advisor', 'Admin', 'Login Management', '0', '0', '8', 'N', 'N', 'advisor', 'user_mng.xhtml', now());
 INSERT INTO invdb.web_menu (url, access, permission, label, level, sublevel, seq, status, availOnMobile, destdir, htmlpage, created) VALUES ('uatbb.invessence.com', 'Advisor', 'Operations', 'Model', '0', '0', '7', 'A', 'N', 'advisor', 'assetworkflow.xhtml',now());
@@ -49,7 +47,7 @@ INSERT INTO invdb.web_menu (url, access, permission, label, level, sublevel, seq
 INSERT INTO invdb.web_menu (url, access, permission, label, level, sublevel, seq, status, availOnMobile, destdir, htmlpage, created) VALUES ('uatbb.invessence.com', 'User', 'User', 'Aggregation', '0', '0', '11', 'N', 'N', '', '',now());
 INSERT INTO invdb.web_menu (url, access, permission, label, level, sublevel, seq, status, availOnMobile, destdir, htmlpage, created) VALUES ('uatbb.invessence.com', 'User', 'User', 'Reports', '0', '0', '11', 'N', 'N', 'consumer', 'creports.xhtml',now());
 
-INSERT INTO invdb.web_menu (url, access, permission, label, level, sublevel, seq, status, availOnMobile, destdir, htmlpage, created) VALUES ('preuob.invessence.com', 'Advisor', 'Admin', 'Login Management', '0', '0', '8', 'N', 'N', 'advisor', 'user_mng.xhtml',now());
+
 INSERT INTO invdb.web_menu (url, access, permission, label, level, sublevel, seq, status, availOnMobile, destdir, htmlpage, created) VALUES ('preuob.invessence.com', 'Advisor', 'Admin', 'Email Logs', '0', '0', '10', 'N', 'N', 'productadmin', 'email_logs.xhtml', now());
 INSERT INTO invdb.web_menu (url, access, permission, label, level, sublevel, seq, status, availOnMobile, destdir, htmlpage, created) VALUES ('preuob.invessence.com', 'Advisor', 'Admin', 'Login Management', '0', '0', '8', 'N', 'N', 'advisor', 'user_mng.xhtml', now());
 INSERT INTO invdb.web_menu (url, access, permission, label, level, sublevel, seq, status, availOnMobile, destdir, htmlpage, created) VALUES ('preuob.invessence.com', 'Advisor', 'Operations', 'Model', '0', '0', '7', 'A', 'N', 'advisor', 'assetworkflow.xhtml',now());
@@ -60,7 +58,7 @@ INSERT INTO invdb.web_menu (url, access, permission, label, level, sublevel, seq
 INSERT INTO invdb.web_menu (url, access, permission, label, level, sublevel, seq, status, availOnMobile, destdir, htmlpage, created) VALUES ('preuob.invessence.com', 'User', 'User', 'Aggregation', '0', '0', '11', 'N', 'N', '', '',now());
 INSERT INTO invdb.web_menu (url, access, permission, label, level, sublevel, seq, status, availOnMobile, destdir, htmlpage, created) VALUES ('preuob.invessence.com', 'User', 'User', 'Reports', '0', '0', '11', 'N', 'N', 'consumer', 'creports.xhtml',now());
 
-INSERT INTO invdb.web_menu (url, access, permission, label, level, sublevel, seq, status, availOnMobile, destdir, htmlpage, created) VALUES ('uatuob.invessence.com', 'Advisor', 'Admin', 'Login Management', '0', '0', '8', 'N', 'N', 'advisor', 'user_mng.xhtml',now());
+
 INSERT INTO invdb.web_menu (url, access, permission, label, level, sublevel, seq, status, availOnMobile, destdir, htmlpage, created) VALUES ('uatuob.invessence.com', 'Advisor', 'Admin', 'Email Logs', '0', '0', '10', 'N', 'N', 'productadmin', 'email_logs.xhtml', now());
 INSERT INTO invdb.web_menu (url, access, permission, label, level, sublevel, seq, status, availOnMobile, destdir, htmlpage, created) VALUES ('uatuob.invessence.com', 'Advisor', 'Admin', 'Login Management', '0', '0', '8', 'N', 'N', 'advisor', 'user_mng.xhtml', now());
 INSERT INTO invdb.web_menu (url, access, permission, label, level, sublevel, seq, status, availOnMobile, destdir, htmlpage, created) VALUES ('uatuob.invessence.com', 'Advisor', 'Operations', 'Model', '0', '0', '7', 'A', 'N', 'advisor', 'assetworkflow.xhtml',now());
