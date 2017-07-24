99607f4db7b6065d75642d1e00cda8c2949add71
sagar <sagar@bd0249eb-a025-43e3-b6fa-fed758350ba6>
2017-07-24 12:21:19 +0000

    Code check in By Sagar for issue fixing

---
+++ Invessence/DBScripts/Release_20170721/Menu/1004.web_site_info_update.sql
@@ -0,0 +1,11 @@
+
+delete from invdb.web_site_info where name='URL.MOBILE' and url =('buildingbenjamins','traditionadvisers','uatbb','uattcm','prebb','pretcm');
+
+INSERT INTO invdb.web_site_info (url, name, status, value, encrFlag, created)values('buildingbenjamins','URL.MOBILE','A','https://BuildingBenjamins.com/mobile.xhtml','N',now());
+INSERT INTO invdb.web_site_info (url, name, status, value, encrFlag, created)values('traditionadvisers','URL.MOBILE','A','https://traditionadvisers.net/mobilelogin.xhtml','N',now());
+INSERT INTO invdb.web_site_info (url, name, status, value, encrFlag, created)values('uatbb','URL.MOBILE','A','http://uatbb.invessence.com:8080/mobile.xhtml','N',now());
+INSERT INTO invdb.web_site_info (url, name, status, value, encrFlag, created)values('uattcm','URL.MOBILE','A','http://uattcm.invessence.com:8080/mobilelogin.xhtml','N',now());
+INSERT INTO invdb.web_site_info (url, name, status, value, encrFlag, created)values('prebb','URL.MOBILE','A','http://prebb.invessence.com:8080/mobile.xhtml','N',now());
+INSERT INTO invdb.web_site_info (url, name, status, value, encrFlag, created)values('pretcm','URL.MOBILE','A','http://pretcm.invessence.com:8080/mobilelogin.xhtml','N',now());
+
+SET SQL_SAFE_UPDATES = 1;
