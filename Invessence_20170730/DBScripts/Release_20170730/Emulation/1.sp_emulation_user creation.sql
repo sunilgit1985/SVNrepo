103358808171bce57d924ab46b12393d84b0885e
sagar <sagar@bd0249eb-a025-43e3-b6fa-fed758350ba6>
2017-07-24 17:03:32 +0000

    commit by sagar for issue fixing

--- Invessence_20170730/DBScripts/Release_20170730/Emulation/1.sp_emulation_user creation.sql
+++ Invessence_20170730/DBScripts/Release_20170730/Emulation/1.sp_emulation_user creation.sql
@@ -2,5 +2,12 @@
 
 use invdb;
 
-insert into invdb.role(logonid,role,status)
-select logonid,'Developer','A' from invdb.user_logon where userid like '%qa.user%';
+INSERT INTO invdb.user_logon ( userid, pwd, logonstatus, lastname, firstname, email, stateRegistered, advisor, rep, emailmsgtype, access, created) VALUES ( 'uob.superadmin', 'c39680887e67fa9860fd197f1279805d', 'A', 'UOB', 'SuperAdmin', 'uob.superadmin@invessence.com', 'NJ', 'UOB', '', 'HTML', 'SuperAdmin', now());
+INSERT INTO invdb.user_logon ( userid, pwd, logonstatus, lastname, firstname, email, stateRegistered, advisor, rep, emailmsgtype, access, created) VALUES ( 'bb.superadmin', 'c39680887e67fa9860fd197f1279805d', 'A', 'BBDemo', 'SuperAdmin', 'bb.superadmin@invessence.com', 'NJ', 'BB', '', 'HTML', 'SuperAdmin', now());
+INSERT INTO invdb.user_logon ( userid, pwd, logonstatus, lastname, firstname, email, stateRegistered, advisor, rep, emailmsgtype, access, created) VALUES ('tcm.superadmin', 'c39680887e67fa9860fd197f1279805d', 'A', 'TCMDemo', 'SuperAdmin', 'tcm.superadmin@invessence.com', 'NJ', 'BB-TCM', '', 'HTML', 'SuperAdmin',now());
+
+insert into invdb.role(logonid,role,status)select logonid,'Operations','A' from invdb.user_logon where userid='bb.advisor';
+
+insert into invdb.role(logonid,role,status)select logonid,'Operations','A' from invdb.user_logon where userid='uob.advisor';
+
+insert into invdb.role(logonid,role,status)select logonid,'Operations','A' from invdb.user_logon where userid='tcm.advisor';
