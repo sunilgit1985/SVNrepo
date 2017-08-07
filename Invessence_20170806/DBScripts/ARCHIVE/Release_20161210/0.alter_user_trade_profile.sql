bc21ee00285dea7821beb64fe5dac760f8c5b93c
sunilt <sunilt@bd0249eb-a025-43e3-b6fa-fed758350ba6>
2017-08-07 08:51:32 +0000

    Moved by sunil

--- Invessence_20170806/DBScripts/ARCHIVE/Release_20161210/0.alter_user_trade_profile.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `invdb`.`user_trade_profile` 
-ADD COLUMN `status` VARCHAR(1) NULL DEFAULT 'N' COMMENT '' AFTER `clientAccountID`,
-ADD COLUMN `customName` VARCHAR(60) NULL DEFAULT NULL COMMENT '' AFTER `status`;
--- Invessence_20170806/DBScripts/ARCHIVE/Release_20161210/03_alter_dc_acct_transfer_details_columnSize.sql
+++
@@ -1,3 +0,0 @@
-use invdb;
-ALTER TABLE dc_acct_transfer_details MODIFY accountTitle VARCHAR(100);
-ALTER TABLE dc_acct_transfer_details MODIFY accountNumber2 VARCHAR(100);
