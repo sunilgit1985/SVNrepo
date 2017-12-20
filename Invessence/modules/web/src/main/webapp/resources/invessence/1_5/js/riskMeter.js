3462ad74eb02498e3d6daae3bff2f87db9195a52
prachi <prachi@bd0249eb-a025-43e3-b6fa-fed758350ba6>
2017-12-20 16:55:42 +0000

    Commited for risk meter

--- Invessence/modules/web/src/main/webapp/resources/invessence/1_5/js/riskMeter.js
+++ Invessence/modules/web/src/main/webapp/resources/invessence/1_5/js/riskMeter.js
@@ -1,90 +1,101 @@
-$(document).ready(function ()
-                                  {
-                                	
-                                	
-                                	                                      var gaugeOptions = {
 
-                                	                                          chart: {
-                                	                                              type: 'solidgauge'
-                                	                                          },
+function myGaugeChart(vMeterVal,vMin,vMax,vLableTop,vLableBottom)	{
+	try{
+		debugger
+		vMeterVal=parseInt(vMeterVal);
+		console.log('vMeterVal~~> '+vMeterVal);
+		console.log('vMin '+vMin);
+		console.log('vMax '+vMax);
+		console.log('vLableTop '+vLableTop);
+		console.log('vLableBottom '+vLableBottom);
+		var gaugeOptions = {
 
-                                	                                          title: null,
+			chart: {
+				type: 'solidgauge'
+			},
 
-                                	                                          pane: {
-                                	                                              center: ['50%', '85%'],
-                                	                                              size: '140%',
-                                	                                              startAngle: -90,
-                                	                                              endAngle: 90,
-                                	                                              background: {
-                                	                                                  backgroundColor: '#EEE',
-                                	                                                  innerRadius: '60%',
-                                	                                                  outerRadius: '100%',
-                                	                                                  shape: 'arc'
-                                	                                              }
-                                	                                          },
+			title: null,
 
-                                	                                          tooltip: {
-                                	                                              enabled: false
-                                	                                          },
+			pane: {
+				center: ['50%', '85%'],
+				size: '140%',
+				startAngle: -90,
+				endAngle: 90,
+				background: {
+					backgroundColor:  '#EEE',
+					innerRadius: '60%',
+					outerRadius: '100%',
+					shape: 'arc'
+				}
+			},
 
-                                	                                          // the value axis
-                                	                                          yAxis: {
-                                	                                              stops: [
-                                	                                                  [0.1, '#55BF3B'], // green
-                                	                                                  [0.5, '#DDDF0D'], // yellow
-                                	                                                  [0.9, '#DF5353'] // red
-                                	                                              ],
-                                	                                              lineWidth: 0,
-                                	                                              minorTickInterval: null,
-                                	                                              tickAmount: 2,
-                                	                                              title: {
-                                	                                                  y: -70
-                                	                                              },
-                                	                                              labels: {
-                                	                                                  y: 16
-                                	                                              }
-                                	                                          },
+			tooltip: {
+				enabled: false
+			},
 
-                                	                                          plotOptions: {
-                                	                                              solidgauge: {
-                                	                                                  dataLabels: {
-                                	                                                      y: 5,
-                                	                                                      borderWidth: 0,
-                                	                                                      useHTML: true
-                                	                                                  }
-                                	                                              }
-                                	                                          }
-                                	                                      };
-                                	                                      
-                                	                                     // ##########
+			// the value axis
+			yAxis: {
+				stops: [
+					[0.1, '#55BF3B'], // green
+					[0.5, '#DDDF0D'], // yellow
+					[0.9, '#DF5353'] // red
+				],
+				lineWidth: 0,
+				minorTickInterval: null,
+				tickAmount: 2,
+				title: {
+					y: -30
+				},
+				labels: {
+					y: 12
+				}
+			},
 
-                                	                                   // The speed gauge
-                                	                                   var chartSpeed = Highcharts.chart('container-speed', Highcharts.merge(gaugeOptions, {
-                                	                                       yAxis: {
-                                	                                           min: 0,
-                                	                                           max: 200,
-                                	                                           title: {
-                                	                                               text: 'Speed'
-                                	                                           }
-                                	                                       },
+			plotOptions: {
+				solidgauge: {
+					dataLabels: {
+						y: 5,
+						borderWidth: 0,
+						useHTML: true
+					}
+				}
+			}
+		};
 
-                                	                                       credits: {
-                                	                                           enabled: false
-                                	                                       },
+		// The speed gauge
+		var chartSpeed = Highcharts.chart('container-speed', Highcharts.merge(gaugeOptions, {
+			yAxis: {
+				min: vMin,
+				max: vMax,
+				title: {
+					text: vLableTop
+				},
+				labels: {
+					distance: -10,
+					//rotation: 'auto'
+				}
+			},
 
-                                	                                       series: [{
-                                	                                           name: 'Speed',
-                                	                                           data: [80],
-                                	                                           dataLabels: {
-                                	                                               format: '<div style="text-align:center"><span style="font-size:25px;color:black">{y}</span><br/>' +
-                                	                                                      '<span style="font-size:12px;color:#000">risk %</span></div>'
-                                	                                           },
-                                	                                           tooltip: {
-                                	                                               valueSuffix: ' '
-                                	                                           }
-                                	                                       }]
+			credits: {
+				enabled: false
+			},
 
-                                	                                   }));
-                                	
-                                	
-                                  });
+			series: [{
+				name: vLableTop,
+				data: [vMeterVal],
+				dataLabels: {
+					format: '<div style="text-align:center;float:bottom;margin-bottom:-100px"><span style="font-size:11px;color:black">{y}</span><br/>' +
+					'<span style="font-size:10px;color:black">'+vLableBottom+'</span></div>'
+				},
+				tooltip: {
+					valueSuffix: vLableBottom
+				}
+			}]
+
+		}));
+
+
+	}catch(e){
+		console.error('Exception '+e);
+	}
+}
