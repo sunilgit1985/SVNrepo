63dbd895f31d24461a60f7a82cacbb3a29cb6b3b
sagar <sagar@bd0249eb-a025-43e3-b6fa-fed758350ba6>
2018-02-25 20:09:05 +0000

    Uploaded by sagar for UOB Prod fixes

---
+++ DBScripts/Release_20180119/UAT-Deploy-2018-02-16/9.sp_invdb_update_web_menu.sql
@@ -0,0 +1,3 @@
+
+UPDATE `invdb`.`web_menu` SET `status`='N' WHERE `url`='utraderobo' and`access`='Advisor' and`permission`='Operations' and`label`='Model';
+UPDATE `invdb`.`web_menu` SET `status`='N' WHERE `url`='uwealth' and`access`='Advisor' and`permission`='Operations' and`label`='Model';
