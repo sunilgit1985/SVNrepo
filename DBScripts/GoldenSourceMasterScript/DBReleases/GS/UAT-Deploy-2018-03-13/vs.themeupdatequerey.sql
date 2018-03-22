62ed6d0e0058ff0e6889f063caa9ec6ac3bbcd85
sunilt <sunilt@bd0249eb-a025-43e3-b6fa-fed758350ba6>
2018-03-22 11:08:12 +0000

    Deleted by sunil

--- DBScripts/GoldenSourceMasterScript/DBReleases/GS/UAT-Deploy-2018-03-13/vs.themeupdatequerey.sql
+++
@@ -1,2 +0,0 @@
-UPDATE `invdb`.`user_basket_access` SET `sortorder`='4' WHERE `advisor`='UOB' and`theme`='8.UOB' and`primary`='Y';
-UPDATE `invdb`.`user_basket_access` SET `sortorder`='0' WHERE `advisor`='UOB' and`theme`='KayHian.Without.Sing' and`primary`='Y';
