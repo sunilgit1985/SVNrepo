1b9c4c2b0536ffffec5e546f31495dac28f8545e
sunilt <sunilt@bd0249eb-a025-43e3-b6fa-fed758350ba6>
2018-03-27 10:14:34 +0000

    Added solution given by abhang

--- DBScripts/Release_20180119_Lockedfor_GSM/UAT-PostDeploy-SP-2018-01-23/6_sp_altr_ext_investment.sql
+++ DBScripts/Release_20180119_Lockedfor_GSM/UAT-PostDeploy-SP-2018-01-23/6_sp_altr_ext_investment.sql
@@ -1,2 +1,16 @@
-ALTER TABLE `invdb`.`ext_investment` 
-ADD COLUMN `status` VARCHAR(25) NULL AFTER `convertedNetAmount`;
+CREATE TABLE `invdb`.`ext_investment` (
+ `acctnum` bigint(20) DEFAULT NULL,
+ `clientAccountID` varchar(12) NOT NULL DEFAULT '',
+ `ticker` varchar(20) NOT NULL DEFAULT 'Cash',
+ `investmentDate` varchar(10) NOT NULL,
+ `status` varchar(1) DEFAULT NULL,
+ `investmentCurrency` varchar(3) NOT NULL DEFAULT 'USD',
+ `netAmount` double DEFAULT NULL,
+ `comment` varchar(120) DEFAULT NULL,
+ `fxRateToBase` double DEFAULT '1',
+ `baseCurrency` varchar(3) DEFAULT 'USD',
+ `convertedNetAmount` double DEFAULT NULL,
+ `created` datetime NOT NULL,
+ PRIMARY KEY (`clientAccountID`,`ticker`,`investmentDate`,`investmentCurrency`),
+ KEY `AK1_ext_transaction` (`acctnum`,`investmentDate`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
