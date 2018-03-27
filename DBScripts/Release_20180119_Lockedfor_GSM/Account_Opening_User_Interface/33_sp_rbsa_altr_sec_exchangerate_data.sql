00273e0da92b3ee33af1cd14f982a80285255377
sunilt <sunilt@bd0249eb-a025-43e3-b6fa-fed758350ba6>
2018-03-27 10:12:35 +0000

    Added solution given by abhang

--- DBScripts/Release_20180119_Lockedfor_GSM/Account_Opening_User_Interface/33_sp_rbsa_altr_sec_exchangerate_data.sql
+++ DBScripts/Release_20180119_Lockedfor_GSM/Account_Opening_User_Interface/33_sp_rbsa_altr_sec_exchangerate_data.sql
@@ -1,2 +1,5 @@
 ALTER TABLE `rbsa`.`sec_exchangerate_data` 
+ADD COLUMN `marketSymbol` VARCHAR(20) NULL AFTER `symbol`;
+
+ALTER TABLE `rbsa`.`sec_exchangerate_data` 
 CHANGE COLUMN `marketSymbol` `marketSymbol` VARCHAR(20) Default NULL ;
